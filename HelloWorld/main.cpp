032de50ff75644cd85ea9a02cfddb89fe32521ae
unknown <zhao.zongbao@zhaozongbao-L-1.bj-oe.21vianet.com>
2014-08-11 07:18:39 +0800

    增加多语言支持 resize

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,13 +1,16 @@
 ﻿#include <QApplication>
 #include <QDialog>
 #include <QLabel>
-
+#include <QTextCodec>
 int main(int argc, char* argv[])
 {
     QApplication App(argc, argv);
+    QTextCodec::setCodecForTr(QTextCodec::codecForLocale());
     QDialog mainWnd;
+    mainWnd.resize(800, 600);
     QLabel label(&mainWnd);
-    label.setText("Hi QT, 我是中国字");
+    label.move(0, 300);
+    label.setText(QObject::tr("Hi QT, 我是中国字"));
     mainWnd.show();
     return App.exec();
 }
