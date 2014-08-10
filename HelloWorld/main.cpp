5e672881355a761b56bc706a6d2a0f4354d6a076
unknown <zhao.zongbao@zhaozongbao-L-1.bj-oe.21vianet.com>
2014-08-11 07:08:14 +0800

    Init

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,13 @@
+﻿#include <QApplication>
+#include <QDialog>
+#include <QLabel>
+
+int main(int argc, char* argv[])
+{
+    QApplication App(argc, argv);
+    QDialog mainWnd;
+    QLabel label(&mainWnd);
+    label.setText("Hi QT, 我是中国字");
+    mainWnd.show();
+    return App.exec();
+}
